af2204b0ef62e13c58a6871eb7dc72b1114e267e
dkm1902 <64354418+dkm1902@users.noreply.github.com>
2024-02-13 14:51:52 +0000

    edited constants

--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path 
 
-CONFIG_FILE_PATH = Path("config/cinfig.yaml") #if not use Path method, forward slash will cause issue
+CONFIG_FILE_PATH = Path("config/config.yaml") #if not use Path method, forward slash will cause issue
 PARAMS_FILE_PATH = Path("params.yaml")
 
